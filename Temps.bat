bff4a1ad0a0321cf78aa85744e0f6ec5f12412d4
Harshith Reddy <89630341+Harshu743@users.noreply.github.com>
2026-01-05 18:26:28 +0530

    actual file

---
+++ Temps.bat
@@ -0,0 +1,9 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+cls
+color a
+del C:\windows\prefetch\*.* /f /s /q
+del C:\windows\temp\*.* /f /s /q
+del %temp%\*.* /f /s /q
+exit
